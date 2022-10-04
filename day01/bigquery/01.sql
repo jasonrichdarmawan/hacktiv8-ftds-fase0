b35dc89247137f385f2fdb638f3f42814834b7ae
Jason <jason.onggo@tempatkerja.com>
2022-10-04 07:43:18 +0700

    add bigquery assignment

---
+++ day01/bigquery/01.sql
@@ -0,0 +1,39 @@
+-- list possible subscriber_type: Subscriber, Customer
+-- SELECT DISTINCT a.subscriber_type
+-- FROM bigquery-public-data.san_francisco.bikeshare_trips AS a
+-- LIMIT 10;
+
+-- list possible landmark: Redwood City
+-- SELECT DISTINCT b.landmark
+-- FROM bigquery-public-data.san_francisco.bikeshare_stations AS b
+-- LIMIT 10;
+
+-- list if the query is valid
+-- SELECT 
+-- a.trip_id, a.start_date, a.subscriber_type, a.start_station_name, a.start_station_id,
+-- b.station_id, b.name, b.landmark
+-- FROM bigquery-public-data.san_francisco.bikeshare_trips AS a
+-- LEFT JOIN bigquery-public-data.san_francisco.bikeshare_stations AS b
+-- ON a.start_station_id = b.station_id
+-- WHERE a.start_date BETWEEN '2016-01-01' AND '2016-03-31'
+-- AND a.subscriber_type = "Subscriber"
+-- AND b.landmark LIKE "Redwood City"
+-- LIMIT 10;
+
+-- Query 1: Count Q1
+SELECT COUNT(trip_id) AS entry
+FROM bigquery-public-data.san_francisco.bikeshare_trips AS a
+LEFT JOIN bigquery-public-data.san_francisco.bikeshare_stations AS b
+ON a.start_station_id = b.station_id
+WHERE a.start_date BETWEEN '2016-01-01' AND '2016-03-31'
+AND a.subscriber_type = "Subscriber"
+AND b.landmark LIKE "Redwood City";
+
+-- Query 2: Count Q2
+SELECT COUNT(trip_id) AS entry
+FROM bigquery-public-data.san_francisco.bikeshare_trips AS a
+LEFT JOIN bigquery-public-data.san_francisco.bikeshare_stations AS b
+ON a.start_station_id = b.station_id
+WHERE a.start_date BETWEEN '2016-04-01' AND '2016-06-30'
+AND a.subscriber_type = "Subscriber"
+AND b.landmark LIKE "Redwood City";
---
+++ day01/bigquery/README.md
@@ -0,0 +1,9 @@
+# dataset from Google Cloud BigQuery
+  1. project_id: bigquery-public-data
+  2. dataset: san_francisco
+  3. table: bikeshare_trips & bikeshare_stations
+  4. assignment link: https://github.com/rafifaditio/h8_set/tree/main/SQL
+
+## ID shorcut:
+   1. bigquery-public-data.san_francisco.bikeshare_trips
+   2. bigquery-public-data.san_francisco.bikeshare_stations
