4ab830aa9b69b324e386420e8977cc8427141238
Jason <jason.onggo@tempatkerja.com>
2022-10-04 16:09:36 +0700

    update comment

--- day02/python_datatypes/main.py
+++ day02/python_datatypes/main.py
@@ -68,10 +68,10 @@ def main():
     Print(list1)
 
     PrintLine()
-    # List is a collection which is ordered and changeable. Allows duplicate members.
-    # Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
-    # Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
-    # Dictionary is a collection which is ordered** and changeable. No duplicate members.
+    # [value] List is a collection which is ordered and changeable. Allows duplicate members.
+    # (value) Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+    # {value} Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+    # {key:value} Dictionary is a collection which is ordered** and changeable. No duplicate members.
     set = {"a", "b"}
     set.add("c")
     Print(set)
